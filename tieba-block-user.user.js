2f0d6138125b3ab5cf621c2d4a11768187f914f3
ccciyo <ccciyo@gmail.com>
2023-10-10 11:51:11 +0800

    Update tieba-block-user.user.js

--- tieba-block-user.user.js
+++ tieba-block-user.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         贴吧屏蔽用户
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://tieba.baidu.com/p/*
